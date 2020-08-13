6a7631e3949158a10640c293475c86e3d62bf790
Labib Hasan <labib.hasan7102@gmail.com>
2020-08-13 19:39:52 +0600

    Updates

---
+++ precache-manifest.a87a96d9261c0f59d9c93d53131b7b58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db41eabaca86ebf945261dda528e03d8",
+    "url": "/kilogram/index.html"
+  },
+  {
+    "revision": "cd349aa57824cd9087df",
+    "url": "/kilogram/static/css/main.c2914539.chunk.css"
+  },
+  {
+    "revision": "4236e6fe5941bb24617a",
+    "url": "/kilogram/static/js/2.19d75fca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kilogram/static/js/2.19d75fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd349aa57824cd9087df",
+    "url": "/kilogram/static/js/main.8db14282.chunk.js"
+  },
+  {
+    "revision": "2f1fdae9d878c082c74f",
+    "url": "/kilogram/static/js/runtime-main.7b7ca307.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kilogram/static/media/logo.5d5d9eef.svg"
+  }
+]);
